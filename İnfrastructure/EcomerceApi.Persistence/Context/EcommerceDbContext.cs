0f4db14e483ec80036d2591e34aac26b9e5a3a51
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-08-10 14:56:54 +0400

    DbContext yaradıldı ve ServiceResgitrationda Serverle əlaqə yaradıldı

---
+++ İnfrastructure/EcomerceApi.Persistence/Context/EcommerceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using EcommerceApi.Domen.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcomerceApi.Persistence.Context
+{
+    public class EcommerceDbContext : DbContext
+    {
+       
+       
+        public EcommerceDbContext(DbContextOptions options) : base(options) { }
+       
+
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        
+    }
+}
